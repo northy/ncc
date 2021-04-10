650ef4f61a9ede37afa0a1306d45217ba3dfe6db
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2021-04-10 20:39:43 -0300

    Changes to syntactical analyzer + basic grammars

--- src/syntactical.py
+++ src/syntactical.py
@@ -1,64 +1,101 @@
 import sys
+from sys import exit
 from collections import deque
 
 def read_grammar(file) :
-    productions = {}
-    production_sizes = []
+    productions = []
+    production_sizes = {}
     actions = {}
     transitions = {}
 
-    grammar_f = open(file, "r")
+    try :
+        grammar_f = open(file, "r")
 
-    i=0
-    for _ in range(int(grammar_f.readline())) :
-        line = grammar_f.readline().split()
-        production_sizes.append(len(line)-2)
-        if line[0] not in productions :
-            productions[line[0]] = i
-            i+=1
-
-    while (line:=grammar_f.readline().split()) :
-        if line[2]=='g' :
-            if int(line[0]) not in transitions :
-                transitions[int(line[0])] = {}
-            transitions[int(line[0])][productions[line[1]]] = int(line[3])
-        else :
-            if int(line[0]) not in actions :
-                actions[int(line[0])] = {}
-            if line[2]=='a' :
-                actions[int(line[0])][line[1]] = (line[2],None)
-            else :
-                actions[int(line[0])][line[1]] = (line[2],int(line[3]))
+        for i in range(int(grammar_f.readline())) :
+            line = grammar_f.readline().split()
+            if line[0] in production_sizes :
+                print("Unexpected error")
+                exit(1)
+            production_sizes[i] = len(line)-2
+            productions.append(line[0])
         
-    grammar_f.close()
+        while (line:=grammar_f.readline().split()) :
+            if line[2]=='g' :
+                if int(line[0]) not in transitions :
+                    transitions[int(line[0])] = {}
+                if line[1] in transitions[int(line[0])] :
+                    print("Unexpected error")
+                    exit(1)
+                transitions[int(line[0])][line[1]] = int(line[3])
+            else :
+                if int(line[0]) not in actions :
+                    actions[int(line[0])] = {}
+                if line[2]=='a' :
+                    actions[int(line[0])][line[1]] = (line[2],None)
+                elif line[2]=='r' :
+                    actions[int(line[0])][line[1]] = (line[2],int(line[3]))
+                else :
+                    actions[int(line[0])][line[1]] = (line[2],int(line[3]))
+        
+        grammar_f.close()
+    except Exception as e :
+        print("Grammar file error on line {}".format(sys.exc_info()[-1].tb_lineno))
+        print(e)
+        exit(1)
 
-    return production_sizes, actions, transitions
+    return productions, production_sizes, actions, transitions
 
-def syntactical(tape, production_sizes, actions, transitions) :
-    tape_f = open(tape, "r")
+def syntactical(tape, productions, production_sizes, actions, transitions) :
+    try :
+        tape_f = open(tape, "r")
 
-    stack = deque()
-    stack.append(0)
+        stack = deque()
+        stack.append(0)
 
-    while (line:=tape_f.readline().split()) :
-        c_state = stack.pop(); stack.append(c_state)
-        act = actions[c_state][line[0]]
-        while act[0]=='r' :
-            for i in range(act[1]) : stack.pop() #reduce
-            stack.append(transitions[stack.top()][act[1]])
+        while (line:=tape_f.readline().split()) :
             c_state = stack.pop(); stack.append(c_state)
+            if c_state not in actions or line[0] not in actions[c_state] :
+                if c_state in actions:
+                    print("Syntax error:", c_state, "doesn't have an action from", line[0])
+                else :
+                    print("Syntax error:", c_state, "has no actions")
+                return False
             act = actions[c_state][line[0]]
-        if act[0]=='a' :
-            return True #accept
-        stack.append(act[1]) #shift
+            while act[0]=='r' :
+                print("Reducing", production_sizes[act[1]], "elements of production", act[1], "from", line[0])
+                for i in range(production_sizes[act[1]]) : stack.pop() #reduce
+                c_state = stack.pop(); stack.append(c_state)
+                print("Transitioning from", c_state, "with", productions[act[1]])
+                if c_state not in transitions or productions[act[1]] not in transitions[c_state] :
+                    if c_state in transitions:
+                        print("Syntax error:", c_state, "doesn't transition from", productions[act[1]])
+                    else :
+                        print("Syntax error:", c_state, "has no transitions")
+                    return False
+                stack.append(transitions[c_state][productions[act[1]]])
+                c_state = stack.pop(); stack.append(c_state)
+                if c_state not in actions or line[0] not in actions[c_state] :
+                    if c_state in actions:
+                        print("Syntax error:", c_state, "doesn't have an action from", line[0])
+                    else :
+                        print("Syntax error:", c_state, "has no actions")
+                    return False
+                act = actions[c_state][line[0]]
+                continue
+            if act[0]=='a' :
+                return True #accept
+            print("Shifting",act[1],"from",line[0])
+            stack.append(act[1]) #shift
 
-    return False
+        return False
 
-    tape_f.close()
+        tape_f.close()
+    except Exception as e :
+        print("Tape file error on line {}".format(sys.exc_info()[-1].tb_lineno))
+        print(e)
+        exit(1)
 
 if __name__=="__main__" :
-    production_sizes, actions, transitions = read_grammar(sys.argv[1])
-    if syntactical("out.lex", production_sizes, actions, transitions) :
+    productions, production_sizes, actions, transitions = read_grammar(sys.argv[1])
+    if syntactical("out.lex", productions, production_sizes, actions, transitions) :
         print("Recognized!")
-    else :
-        print("Not recognized :(")
