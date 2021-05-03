8ba68db054b622fdcfb5bbae7d439b0eb17f0315
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2021-05-03 19:09:27 -0300

    Adding uninitialized variables warnings to sem.

--- src/semantical.py
+++ src/semantical.py
@@ -20,19 +20,19 @@ compatible = {
 
 def get_type(symbol, variables, regs, program, l, c) :
     if symbol.startswith("%") :
-        return regs[symbol]
+        return regs[symbol], True
 
     try :
         int(symbol)
-        return "int"
+        return "int", True
     except : pass
 
     try :
-        return variables[symbol]["type"]
+        return variables[symbol]["type"], variables[symbol]["init"]
     except :
         print("Semantical error: variable used but not declared:", symbol)
         semantical_error(program, int(l), int(c))
-        return False
+        return False, True
 
 def semantical_error(file, l, c) :
     line = linecache.getline(file, l+1)
@@ -53,10 +53,17 @@ def semantical(file, program, debug=False) :
     while (line:=symbolt_f.readline()) :
         line = line.split()
         if line[0]=="O" :
-            typea = get_type(line[3], variables, regs, program, line[5], line[6])
-            typeb = get_type(line[4], variables, regs, program, line[5], line[6])
+            typea, inita = get_type(line[3], variables, regs, program, line[5], line[6])
+            typeb, initb = get_type(line[4], variables, regs, program, line[5], line[6])
 
             if not(typea) or not(typeb) : return False
+
+            if not(inita) :
+                print(f"Semantical warning: {line[3]} ({typea}) used but possibly unitialized")
+                semantical_error(program, int(line[5]), int(line[6]))
+            if not(initb) :
+                print(f"Semantical warning: {line[4]} ({typeb}) used but possibly unitialized")
+                semantical_error(program, int(line[5]), int(line[6]))
 
             if line[1] not in operations_supported[typea][typeb] :
                 print(f"Semantical error: Can't use operation {line[1]} between {line[3]} ({typea}) and {line[4]} ({typeb})")
@@ -66,12 +73,19 @@ def semantical(file, program, debug=False) :
 
         elif line[0]=="D" :
             variables[line[1]] = {
-                "type": line[2]
+                "type": line[2],
+                "init": False
             }
 
         elif line[0]=="S" :
-            typea = get_type(line[1], variables, regs, program, line[3], line[4])
-            typeb = get_type(line[2], variables, regs, program, line[3], line[4])
+            typea,inita = get_type(line[1], variables, regs, program, line[3], line[4])
+            typeb,initb = get_type(line[2], variables, regs, program, line[3], line[4])
+
+            if not(inita) :
+                variables[line[1]]["init"] = True
+            if not(initb) :
+                variables[line[2]]["init"] = True
+                
 
             if typeb not in compatible[typea] :
                 print(f"Semantical error: Can't store {line[2]} ({typeb}) in {line[1]} ({typea})")
